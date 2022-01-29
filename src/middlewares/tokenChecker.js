6f50f77c6cc4c81ca0fd6a1e26e32d737ccc8f61
Eric <encsngl@gmail.com>
2022-01-30 01:07:12 +0800

    Removed code

--- src/middlewares/tokenChecker.js
+++ src/middlewares/tokenChecker.js
@@ -19,7 +19,6 @@ module.exports = (req, res, next) => {
       next();
     });
   } else {
-    // return res.status(400).json(jsonResponse("No token provided.", false));
     return res.status(400).json(jsonResponse("Authentication failed.", false));
   }
 };
